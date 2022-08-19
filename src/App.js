225d8467f35812ddf13ed46f7d162395511183c3
Roberto <ticoda12@pop-os.localdomain>
2022-08-19 16:03:28 -0300

    qrcode generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import QrCode from 'react-qr-code'
+import QrCodeLink from 'qrcode'
+
 import './App.css';
 
 function App() {
+  const [link, setlink] = useState('')
+  const [qrTemp, setQrTemp] = useState('')
+
+  function handleGenarate(link_url){
+    QrCodeLink.toDataURL(link_url,{
+      width: 800,
+      margin: 4, 
+    }, function (err, url){
+      setQrTemp(url);
+    })
+  }
+
+  function handleQrcode(event){
+    setlink(event.target.value);
+    handleGenarate(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' >
+
+      <QrCode
+        value={link}
+      />  
+
+      <input
+        className='input' 
+        placeholder='Digitar o que deseja transformar em qrcode!' 
+        value={link}
+        onChange={ (event)=> handleQrcode(event)}
+        /> 
+
+      <a href={qrTemp} download={`qrcode.png`} > Download QrCode </a>
+
     </div>
   );
 }
